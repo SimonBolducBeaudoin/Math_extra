4fb45d9e9891807cd84a34c2418d4ba78c346bf7
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-03 11:21:35 -0400

    Beautifying clang-format

--- src/math_extra.cpp
+++ src/math_extra.cpp
@@ -1,27 +1,24 @@
 #include "math_extra.h"
 
-uint64_t n_choose_k(uint n, uint k)
-{
-    if (k==0)
-    {
-        return 1;
-    }
-    return (n*n_choose_k(n-1, k-1))/k; // Product form, division always yields an integer
+uint64_t n_choose_k(uint n, uint k) {
+  if (k == 0) {
+    return 1;
+  }
+  return (n * n_choose_k(n - 1, k - 1)) /
+         k; // Product form, division always yields an integer
 }
 
-
 /*
-from : https://www.geeksforgeeks.org/check-if-given-number-is-perfect-square-in-cpp/
+from :
+https://www.geeksforgeeks.org/check-if-given-number-is-perfect-square-in-cpp/
 */
-bool isPerfectSquare(long double x) 
-{   
-  // Find floating point value of  
-  // square root of x. 
-  long double sr = sqrt(x); 
-  
-  // If square root is an integer 
-  return ((sr - floor(sr)) == 0); 
-} 
+bool isPerfectSquare(long double x) {
+  // Find floating point value of
+  // square root of x.
+  long double sr = sqrt(x);
+
+  // If square root is an integer
+  return ((sr - floor(sr)) == 0);
+}
 
 // void linear_vertical_least_square():
-	
--- src/math_extra.h
+++ src/math_extra.h
@@ -1,11 +1,9 @@
 #pragma once
-#include <stdint.h>
 #include <cmath>
+#include <stdint.h>
 
 typedef unsigned int uint;
 
 uint64_t n_choose_k(uint n, uint k);
 
 bool isPerfectSquare(long double x);
-
-
